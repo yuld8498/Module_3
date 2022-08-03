a85736544d5b4ad31f748d134858b190125abf36
yuld8498 <truongthanh11201@gmail.com>
2022-08-03 21:49:44 +0700

    add

---
+++ Servlet/src/main/java/com/codygym/servlet/Simple_login.java
@@ -0,0 +1,38 @@
+package com.codygym.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "LoginServlet", value = "/login")
+public class Simple_login extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html>");
+
+        if ("admin".equals(username) && "admin".equals(password)) {
+            writer.println("<h1>Welcome " + username + " to website</h1>");
+        } else {
+            writer.println("<h1>Login Error</h1>");
+        }
+
+        writer.println("</html>");
+    }
+
+    @Override
+    public void init() throws ServletException {
+
+    }
+}
